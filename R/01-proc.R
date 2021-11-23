664c486fb82df59d4d6c3b9bc5fd84f7dc6e78f1
vicente opazo cortés <vichopazo12@gmail.com>
2021-11-23 02:51:48 -0300

    Update_01_datos_proc
    Push BDD procesada (para descargar vía url en el documento Rmarkdown!)

---
+++ R/01-proc.R
@@ -0,0 +1,102 @@
+#Cargar paquetes
+
+library(pacman)
+
+pacman::p_load(tidyverse,
+               sjmisc,
+               srvyr,
+               tidyr, 
+               dplyr, 
+               sjPlot, 
+               car)
+
+#Cargar y renombrar BDD (Encuesta nacional de empleo 2021_08, j-a-s) 
+
+ENE_2021_08 <- read_sav(url("https://ine.cl/docs/default-source/ocupacion-y-desocupacion/bbdd/2021/spss/ene-2021-08-jas.sav?sfvrsn=a5e96065_4&download=true")) 
+
+#Inspección inicial BDD
+
+head(ENE_2021_08)
+view_df(ENE_2021_08)
+
+#Explorar BDD 1: Encontrar variables de interés
+
+find_var(ENE_2021_08, "sexo") #nr col: 15
+find_var(ENE_2021_08, "cine") #nr col: 173 
+find_var(ENE_2021_08, "edad") #nr col: 177 (select==tramo_edad)
+find_var(ENE_2021_08, "activ") #nr col: 178
+find_var(ENE_2021_08, "e12") #nr col: 125 -motivos de no estar disponible para trabajar
+find_var(ENE_2021_08, "e9") #nr col: 119 -razón de no buscar empleo
+
+#Explorar BDD 2: Encontrar variables de identificación, estrato y ponderación
+
+find_var(ENE_2021_08, "id_identificacion") #nr col: 8
+find_var(ENE_2021_08, "estrato") #nr col: 5
+find_var(ENE_2021_08, "fact_cal") #nr col: 181
+
+#Explorar BDD 3: Definir clase variables de interés
+
+class("sexo")
+class("cine")
+class("tramo_edad")
+class("activ")
+class("e12")
+class("e9") #all==character
+
+#Explorar casos variables analíticas
+
+frq(ENE_2021_08$sexo)
+frq(ENE_2021_08$cine)
+frq(ENE_2021_08$tramo_edad)
+frq(ENE_2021_08$activ)
+frq(ENE_2021_08$e12)
+frq(ENE_2021_08$e9)
+
+#Crear objeto BBD procesada, seleccionar y renombrar variables de interés
+
+datos_proc <- select(ENE_2021_08, sexo, cine, edad, edad_tramo = tramo_edad, activ, 
+                     motiv_no_dis_trab = e12, motiv_no_bus_trab = e9, id = id_identificacion, 
+                     estrato, fact_exp = fact_cal)
+
+view_df(datos_proc) 
+
+#Recodificación de variables de interés (saturar categorías_1)
+
+datos_proc <- datos_proc %>% 
+  filter(edad >= 15) %>%
+  mutate(edad_tramo = case_when(edad_tramo >= 1 & edad_tramo <= 3 ~ "Joven", edad_tramo > 3 & 
+                                      edad_tramo <= 9 ~ "Adulto/a", edad_tramo > 9 ~ "Adulto/a mayor", 
+                                    TRUE ~ NA_character_))
+
+datos_proc <- datos_proc %>% 
+  mutate(activ = case_when(activ >= 1 & activ < 3 ~ "Dentro de la fuerza de trabajo", activ >= 3 ~ 
+                                            "Fuera de la fuerza de trabajo", TRUE ~ NA_character_))
+
+datos_proc <- datos_proc %>% 
+  mutate(motiv_no_dis_trab = if_else(motiv_no_dis_trab %in% c(5,6,7), "Es pensionado/a, jubilado/a o rentista", 
+                         if_else(motiv_no_dis_trab %in% c(3), "Porque está estudiando o empezará a estudiar pronto",
+                         if_else(motiv_no_dis_trab %in% c(1,2,10,12,13), "Otra razón", 
+                         if_else(motiv_no_dis_trab %in% c(4,11), "Por responsabilidades familiares (cuidado de 
+                                 niños o personas dependientes) permanentes o temporales", 
+                         if_else(motiv_no_dis_trab %in% c(8,9), "Por motivos de salud permanentes o temporales", 
+                                 NA_character_))))))
+
+datos_proc <- datos_proc %>% 
+  mutate(motiv_no_bus_trab = if_else(motiv_no_bus_trab %in% c(5,6,7), "Es pensionado/a, jubilado/a o rentista", 
+                             if_else(motiv_no_bus_trab %in% c(4), "Porque está estudiando o preparando estudios", 
+                             if_else(motiv_no_bus_trab %in% c(1,2,9,12,13,14,15,16,17,18,19,20,21,22), "Otra razón", 
+                             if_else(motiv_no_bus_trab %in% c(3,11), "Por responsabilidades familiares (cuidado de 
+                                 niños o personas dependientes) permanentes o temporales", 
+                             if_else(motiv_no_bus_trab %in% c(8,10), "Por motivos de salud permanentes o temporales", 
+                                                             NA_character_))))))
+
+#Limpieza variables de interés (definición NAs para valor "999" para var. cine)
+
+datos_proc[datos_proc == 999] <- NA                                              
+
+#Guardar BDD procesada
+
+saveRDS(datos_proc, file= "output/data/datos_proc.rds")
+
+
+
---
+++ README_examen.md
@@ -0,0 +1,12 @@
+#Fuentes
+
+https://www.ine.cl/ine-ciudadano/definiciones-estadisticas/poblacion/caracteristicas-de-la-poblacion
+
+#Algunas consideraciones metodológicas
+
+<div style="text-align: justify"> 1-Se aplicó el filtro para excluir a personas menores de 15 años en consideración de que estas no se encuentran, según las definición de...en edad de trabajar.
+
+2-Las **variables de interés** se definieron en función de: a) la literatura previa revisada. De la teorización de Castillo... [selección sexo, empleo, etc.] y b) las definiciones teórico-metodológicas del INE respecto a la diferencia entre población desempleada y población fuera de la fuerza de trabajo, según la cual [Libro de Códigos, pág. 52-53]... Asimismo, la saturación de categorías se realizó en función de: a) los criterios analíticos contenidos en la literatura previa, y b) la revisión de las frecuencias porcentuales de cada categoría de las variables de interés en la BDD original (e. g., la categoría "Por responsabilidades familiares permanentes (cuidado de niños o personas dependientes)" correspondientes a la variable "Razones para no buscar empleo el último mes" posee un 23,24% de los casos válidos en la BDD original).
+
+3-La alta proporción de NA en la variable e12: "*¿Por qué motivo no estaría disponible para trabajar?*" (65,5%), se debe a que la pregunta anterior en la encuesta original (e11 en *ENE-2021-08*) corresponde a: "*Si durante la semana pasada hubiera encontrado un trabajo, ¿estaría disponible para comenzar a trabajar de este lunes que viene al otro?*". Es por esto que la versión recodificada de e12 contenida en la BBD procesada (i. e., "motiv_nodis_trab"), al igual que la variable original, presenta una relativamente baja frecuencia de casos. <div/>
+
